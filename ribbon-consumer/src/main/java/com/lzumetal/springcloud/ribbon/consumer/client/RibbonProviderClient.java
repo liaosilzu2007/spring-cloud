a1fe1651d20903aced61739f61806dc11d16a465
liaosilzu2007 <liaosilzu2007@163.com>
2021-04-07 18:37:45 +0800

    # ribbon

--- ribbon-consumer/src/main/java/com/lzumetal/springcloud/ribbon/consumer/client/RibbonProviderClient.java
+++ ribbon-consumer/src/main/java/com/lzumetal/springcloud/ribbon/consumer/client/RibbonProviderClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author liaosi
  * @date 2021-04-06
  */
-@FeignClient(name = "ribbon-provider")
+@FeignClient(name = "ribbon-provider", path = "/ribbonProvider")
 public interface RibbonProviderClient {
 
     @GetMapping("/hello")
